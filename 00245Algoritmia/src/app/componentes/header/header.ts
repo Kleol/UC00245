25f79ad04a080837f9fed3fc87880894a9c069a8
natt <natkweo@gmail.com>
2025-10-28 15:20:19 +0000

    v2

--- 00245Algoritmia/src/app/componentes/header/header.ts
+++ 00245Algoritmia/src/app/componentes/header/header.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterModule } from "@angular/router";
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule], // assegura que o CommonModule está disponível
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
--- 00245Algoritmia/src/app/componentes/main/main.ts
+++ 00245Algoritmia/src/app/componentes/main/main.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-main',
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet], 
   templateUrl: './main.html',
   styleUrl: './main.css'
 })
 export class Main {
 
 }
+
+
